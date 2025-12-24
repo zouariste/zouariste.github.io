a783e539df94ca0c2e5cd7fee86a983bda2edc2c
I566972 <mohamed.zouari@sap.com>
2025-12-24 13:19:21 +0100

    Good version, need to fix profile photos

--- tools/meta-extractor/script_block_list.py
+++ tools/meta-extractor/script_block_list.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import base64
 
 # Load your HTML
-with open("blocks.html", "r", encoding="utf-8") as f:
+with open("sample-blocks.html", "r", encoding="utf-8") as f:
     html = f.read()
 
 soup = BeautifulSoup(html, "html.parser")
